f7e0889a7a8c19acd0d0c07a71ac4beeac1b1985
arnoldbgm <arnold.gallegosm@gmail.com>
2025-04-12 20:18:59 -0500

    ✨Feat: Flask + React

--- 03-FlaskRender/backend/app.py
+++ 03-FlaskRender/backend/app.py
@@ -5,12 +5,16 @@ from flask_migrate import Migrate
 from models.categorias import CategoriaModel #Es mi tabla de categorias
 from models.post import PostModel #Es mi tabla de post
 from routes.categoria_routes import CategoriaListResource
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
 
 # Creamos la instancia de flask
 app = Flask(__name__)
+CORS(app)
 # Vamos a conectarnos con nuestra base de datos (SQLAlchemy)
 # Debes de usar el config de tu instancia de Flask
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ugILcasQvyFcfXqPvsfGdVOAPFYPhBLQ@mainline.proxy.rlwy.net:29060/railway'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
 
 # Configuramos la base de datos
 db.init_app(app)
